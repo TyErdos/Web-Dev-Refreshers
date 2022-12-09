9e0383fb0da5427aa8f354518e739b6725261bce
TyErdos <44851615+TyErdos@users.noreply.github.com>
2022-12-09 13:17:27 -0500

    Concept of truthy and falsy

---
+++ JavaScript/Truthy and Falsy/app.js
@@ -0,0 +1,39 @@
+// const userInput = prompt("Enter something");
+
+// if (userInput)
+// {
+//     console.log('Truthy');
+// }
+// else 
+// {
+//     console.log('Falsy');
+// }
+
+
+// if (0)
+// {
+//     console.log('Truthy');
+// }
+// else 
+// {
+//     console.log('Falsy');
+// }
+
+
+// if(undefined)
+// {
+//     console.log('Truthy');
+// }
+// else 
+// {
+//     console.log('Falsy');
+// }
+
+if(' ')
+{
+    console.log('Truthy');
+}
+else 
+{
+    console.log('Falsy');
+}
