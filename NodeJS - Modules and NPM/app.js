ea19269ff33a15f52bf83e457aeb033e08e2194a
TylerErdos <tse9@psu.edu>
2023-02-01 16:13:13 -0500

    NodeJS - Modules and NPM

---
+++ NodeJS - Modules and NPM/app.js
@@ -0,0 +1,11 @@
+// const math = require('./math');
+
+// const {PI, square} = require('./math');
+
+// console.log(math);
+
+// console.log(math.square(9));
+
+const cats = require('./shelter');
+
+console.log("Required an entire directory", cats)
