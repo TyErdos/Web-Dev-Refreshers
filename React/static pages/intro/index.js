712722af9cff1062edf6a10dd7781df1f4f6b0a1
TyErdos <erdostyler@gmail.com>
2023-05-03 15:42:22 -0400

    All React Content

---
+++ React/static pages/intro/index.js
@@ -0,0 +1,3 @@
+ //allows rendering of stuff that looks like html
+ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+ReactDOM.render(<h2>Hi</h2>, document.getElementById("root"));
