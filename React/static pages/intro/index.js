f7d9f77ac7d69d75f331c4ac94bb36d2d6aa2af4
TyErdos <44851615+TyErdos@users.noreply.github.com>
2023-05-03 15:51:15 -0400

    Delete React/static pages/intro directory

--- React/static pages/intro/index.js
+++
@@ -1,3 +0,0 @@
- //allows rendering of stuff that looks like html
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
-ReactDOM.render(<h2>Hi</h2>, document.getElementById("root"));
